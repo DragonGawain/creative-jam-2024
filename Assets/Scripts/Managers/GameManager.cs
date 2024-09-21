7f9c21cf6e3f4d8d2c3fc14ed4b6943f1b232738
DragonGawain <craigk00@hotmail.com>
2024-09-20 22:22:14 -0400

    mostly functional character controller

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static event Action NextGameTick;
+
+    Grid grid;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        grid = GameObject.FindWithTag("Grid").GetComponent<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerNextGameTick()
+    {
+        NextGameTick?.Invoke();
+    }
+
+    public Vector3 Move(Vector3 pos, Vector2Int dir)
+    {
+        return grid.CellToWorld(grid.WorldToCell(pos) + new Vector3Int(dir.x, dir.y, 0))
+            + new Vector3(0.5f, 0.5f, 0);
+    }
+
+
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,17 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
+    Inputs inputs;
+    Vector2 movementInput;
+
+    GameManager gameManager; // could just serialize this?
+
+    bool canMove = true;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        inputs = new Inputs();
+        inputs.Player.Enable();
+        inputs.Player.Move.performed += Move;
+        inputs.Player.Move.canceled += EndMove;
+
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Move(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        if (!canMove)
+            return;
+
+        movementInput = inputs.Player.Move.ReadValue<Vector2>();
+        if (movementInput.x > 0)
+        {
+            transform.position = gameManager.Move(transform.position, new Vector2Int(1,0));
+            // move right
+        }
+        else if (movementInput.x < 0)
+        {
+            transform.position = gameManager.Move(transform.position, new Vector2Int(-1,0));
+            // move left
+        }
+        else if (movementInput.y > 0)
+        {
+            transform.position = gameManager.Move(transform.position, new Vector2Int(0,1));
+            // move up
+        }
+        else if (movementInput.y < 0)
+        {
+            transform.position = gameManager.Move(transform.position, new Vector2Int(0,-1));
+            // move down
+        }
+
+        canMove = false;
+
+        gameManager.TriggerNextGameTick();
+
+    }
+
+    void EndMove(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        if (inputs.Player.Move.ReadValue<Vector2>().magnitude == 0)
+            canMove = true;
+    }
+
+    void OnDestroy()
+    {
+        inputs.Player.Move.performed -= Move;
+        inputs.Player.Move.canceled -= EndMove;
     }
 }
